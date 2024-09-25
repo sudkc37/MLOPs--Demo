eca6fdc337e1a97f4d583d7fde2230be702faaa4
Sudip Khadka <sudipkhadka@Sudips-MBP.attlocal.net>
2024-09-25 09:48:44 -0500

    exception, logger & utils added

---
+++ main.py
@@ -0,0 +1,3 @@
+from mlProject import logger
+
+logger.info("Wellcome to the Prediction Universe")
---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,58 @@
+from mlproject import logger
+from json
+import joblib
+from ensuer import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories:list, verbose=True):
+    for path in path_to_directories:
+        os.makerdirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+@ansure_annotations
+def save_json(path:Path, data:dict):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    
+    logger.info(f"json file saved at: {path}")
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+    logger.info(f"json file loaded succesfully from: {path}")
+    return ConfigBox(content)
+
+@ensuer_annotations
+def save_bin(data: Any, path: Path):
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    data = joblib.load(path)
+    logger.info(f"binary file loaded from: {path}")
+    return data
+
+@ensure_annotations 
+def get_size(path: path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"
+    
